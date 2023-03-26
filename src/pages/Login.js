0071d3ee9fa52930b0444de6213b533ccb1c5681
CommanderW324 <winstoncahya90@gmail.com>
2023-03-26 15:08:08 +0800

    Initial Webpage

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+function Login() {
+  return (
+    <h1>
+        Hello World
+    </h1>
+  );
+}
+
+export default Login;
